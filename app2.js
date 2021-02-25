f75a5e10adc688a39f012c93ec51f9da49794316
aies-americanschool <79632879+aies-americanschool@users.noreply.github.com>
2021-02-25 18:16:10 +0900

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,15 @@
+// const hambuger = document.querySelector('.hamburger');
+// const navlinks = document.querySelector('.nav-links');
+// const links = document.querySelector('.nav-links li');
+
+// hambuger.addEventListener('click', () => {
+//   navlinks.classList.toggle('open');
+// });
+
+const hamburger = document.querySelector('.hamburger');
+const navlinks = document.querySelector('.nav-links');
+const links = document.querySelector('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+  navlinks.classList.toggle('open');
+});
